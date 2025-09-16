8ace440daa47f7135d607349f6fd4f13f67993df
matija-code <milincicmatija9@gmail.com>
2025-09-16 10:09:30 +0200

    Popravni commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,49 +1,63 @@
+<!DOCTYPE html>
+<html>
 
-        <!DOCTYPE html>
-        <html>
-            <head>
-                <meta charset="utf-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="description" content="">
-                <meta name="viewport" content="width=device-width, initial-scale=1">
-                <link rel="stylesheet" href="{{ asset ('home.css')}}">
-                <title>home</title>
-                <style>
-                    body,html{overflow-x:hidden}
-                    .flex-parent{display:flex}
-                </style>
-            </head>
-            <body>
-        <p class="usi-hello-matija">Hello Matija</p><p class="usi-haven-t-purchased-a">Haven’t purchased a ticket with us?
-Input your ticket and let us help you find
-for you.</p><div class="usi-group-57"> 
- <div class="usi-rectangle-15"></div> 
- <p class="usi-airplane-ticket">Airplane ticket</p>
-</div><div class="usi-rectangle-145">	<div class="usi-rectangle-5"></div>	<p class="usi-search-destinations">Search Destinations</p>	<div class="usi-group-58"> 
- <p class="usi-suggested">Suggested destinations</p> 
- <div class="usi-group-56"> 
- <div class="usi-group-55"> 
- <div class="usi-rectangle-13"></div> 
- <p class="usi-more-of-suggested">More of Suggested destinations</p>
-</div> 
- <div class="usi-group-52"> 
- <div class="usi-rectangle-9"></div> 
- <p class="usi-kuala-lumpur">Kuala Lumpur</p>
-</div> 
- <div class="usi-group-51"> 
- <div class="usi-rectangle-10"></div> 
- <p class="usi-paris">Paris</p>
-</div> 
- <div class="usi-group-53"> 
- <div class="usi-rectangle-11"></div> 
- <p class="usi-san-francisco">San Francisco</p>
-</div> 
- <div class="usi-group-54"> 
- <div class="usi-rectangle-12"></div> 
- <p class="usi-london">London</p>
-</div>
-</div>
-</div></div>
-            </body>
-        </html>
-        
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{asset ('home.css')}}">
+    <title>home</title>
+    <style>
+        body,
+        html {
+            overflow-x: hidden
+        }
+
+        .flex-parent {
+            display: flex
+        }
+    </style>
+</head>
+
+<body>
+    <p class="usi-hello-matija">Hello Matija</p>
+    <p class="usi-haven-t-purchased-a">Havenï¿½t purchased a ticket with us?
+        Input your ticket and let us help you find
+        for you.</p>
+    <div class="usi-group-57">
+        <div class="usi-rectangle-15"></div>
+        <p class="usi-airplane-ticket">Airplane ticket</p>
+    </div>
+    <div class="usi-rectangle-145">
+        <div class="usi-rectangle-5"></div>
+        <p class="usi-search-destinations">Search Destinations</p>
+        <div class="usi-group-58">
+            <p class="usi-suggested">Suggested destinations</p>
+            <div class="usi-group-56">
+                <div class="usi-group-55">
+                    <div class="usi-rectangle-13"></div>
+                    <p class="usi-more-of-suggested">More of Suggested destinations</p>
+                </div>
+                <div class="usi-group-52">
+                    <div class="usi-rectangle-9"></div>
+                    <p class="usi-kuala-lumpur">Kuala Lumpur</p>
+                </div>
+                <div class="usi-group-51">
+                    <div class="usi-rectangle-10"></div>
+                    <p class="usi-paris">Paris</p>
+                </div>
+                <div class="usi-group-53">
+                    <div class="usi-rectangle-11"></div>
+                    <p class="usi-san-francisco">San Francisco</p>
+                </div>
+                <div class="usi-group-54">
+                    <div class="usi-rectangle-12"></div>
+                    <p class="usi-london">London</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
