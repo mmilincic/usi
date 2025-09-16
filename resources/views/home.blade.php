26ffa7dc0f9f47ec68c62efce3a2f8d802682c42
matija-code <milincicmatija9@gmail.com>
2025-09-16 09:52:37 +0200

    Popravni commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
                 <meta http-equiv="X-UA-Compatible" content="IE=edge">
                 <meta name="description" content="">
                 <meta name="viewport" content="width=device-width, initial-scale=1">
-                <link rel="stylesheet" href="{{ asset home.css}}">
+                <link rel="stylesheet" href="{{asset ('home.css')}}">
                 <title>home</title>
                 <style>
                     body,html{overflow-x:hidden}
@@ -14,7 +14,7 @@
                 </style>
             </head>
             <body>
-        <p class="usi-hello-matija">Hello Matija</p><p class="usi-haven-t-purchased-a">Haven’t purchased a ticket with us?
+        <p class="usi-hello-matija">Hello Matija</p><p class="usi-haven-t-purchased-a">Havenï¿½t purchased a ticket with us?
 Input your ticket and let us help you find
 for you.</p><div class="usi-group-57"> 
  <div class="usi-rectangle-15"></div> 
